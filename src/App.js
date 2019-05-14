8e41f99f327b54458f76d0441c5e6b31fd112253
Mikazuki_Laisa <laisarinvalkryflare@gmail.com>
2019-05-14 18:28:41 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,164 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { nullLiteral } from '../node_modules/@babel/types';
+import { ENETDOWN } from 'constants';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Model2 />
     </div>
   );
+}
+
+class ValueInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.onNameChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <fieldset>
+        <legend>Enter {this.props.title} in ababa:</legend>
+        <input value={this.props.value}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+class Model2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleContentsChange = this.handleContentsChange.bind(this);
+    this.state = {name: '', contents: ""};
+  }
+
+  handleNameChange(name) {
+    this.setState({name});
+  }
+  handleContentsChange(contents) {
+    this.setState({contents});
+  }
+
+  render() {
+    return (
+      <div>
+        <ValueInput
+          title="name"
+          value={this.state.name}
+          onNameChange={this.handleNameChange} />
+          {this.state.name}
+        <ValueInput
+          title="contents"
+          value={this.state.contents}
+          onNameChange={this.handleContentsChange} />
+          {this.state.contents}
+      </div>
+    );
+  }
+}
+
+
+
+let scheduleList = [];
+let itemList = [];
+class Model extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      name: "",
+      date: "",
+      contents: ""
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+  };
+
+  handleNameChange(value) {
+    this.setState({name: value});
+  }
+
+  render(){
+    const name = "Laisa"
+    return(
+      <div>
+        {this.state.name}
+        {this.state.date}
+        {this.state.contents}
+        <InputField
+            type="name"
+            value={name}
+            onValueChange={this.handleValueChange} />
+      </div>
+    )
+  }
+}
+
+class InputField extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    //this.setState({value: event.target.value});
+    this.props.onValueChange(event.target.value)
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    const name = this.props.value;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          {this.props.type}:
+          <input type="text" value={name} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class Schedule extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "ababa",
+      date: "hogege",
+      contents: "huga"
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>
+          {this.state.name}
+        </h1>
+        <label>
+          {this.state.date}
+        </label>
+        <br />
+        <textarea rows="10" cols="60">
+          {this.state.contents}
+        </textarea>
+      </div>
+    )
+  }
 }
 
 export default App;
